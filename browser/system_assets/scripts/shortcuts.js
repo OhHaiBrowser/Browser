08a04bf0524e3850330aedc59cade77cc6967be3
Lucas <Lucas.Reade@yahoo.com>
2019-01-16 22:09:34 +0000

    Miscellaneous code changes.

---
+++ browser/system_assets/scripts/shortcuts.js
@@ -0,0 +1,22 @@
+var {globalShortcut} = require('electron');
+
+
+globalShortcut.register('F5', () => {
+    
+});
+
+globalShortcut.register('Alt+Left', () => {
+    
+});
+globalShortcut.register('Alt+Right', () => {
+    
+});
+
+
+globalShortcut.register('Alt+Up', () => {
+    
+});
+globalShortcut.register('Alt+Down', () => {
+    
+});
+
