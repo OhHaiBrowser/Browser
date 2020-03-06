d7f75f1f63af821fae2e01d2db79d0bd0854fbf9
Lucas Reade <lucas.reade@yahoo.com>
2020-03-06 15:39:29 +0000

    Update to urlbar

--- browser/components/url-bar/urlbar.component.js
+++ browser/components/url-bar/urlbar.component.js
@@ -3,13 +3,13 @@ class UrlBar extends HTMLElement {
 		super();
 		const shadowEl = this.attachShadow({mode: 'open'});
 		shadowEl.innerHTML = `
-			<link rel='stylesheet' href='${__dirname}/urlbar.component.css' />
+			<link rel='stylesheet' href='style.css' />
 			<div class="componentOuter">
 				<div class="urlOuter">
-					<a id="SecureCheck" class="DoubleURLBtn">&#xE72E;</a>
+					<a id="SecureCheck" class="DoubleURLBtn Internal"></a>
 					<input class="URLBar" type="text" id="URLBar" />
-					<button id="BtnQuicklink" class="URLButton">&#xE734;</button>
-					<button id="Refresh" title="Refresh this page" class="URLButton Refresh">&#xE72C;</button>             
+					<button id="BtnQuicklink" class="URLButton"></button>
+					<button id="Refresh" title="Refresh this page" class="URLButton Refresh"></button>             
 				</div>
 				<div id="URLAutoComplete" class="URL_AutoComplete AutoComplete_hidden"></div>
 			</div>
@@ -17,8 +17,10 @@ class UrlBar extends HTMLElement {
 		this.shadowRoot.getElementById('SecureCheck').addEventListener('click', () => {
 
 		});
-		this.shadowRoot.getElementById('BtnQuicklink').addEventListener('click', () => {
-			this.dispatchEvent(new Event('favorited', { detail: true }));
+		let btnFav = this.shadowRoot.getElementById('BtnQuicklink');
+		btnFav.addEventListener('click', () => {
+			btnFav.classList.toggle('active');
+			this.dispatchEvent(new Event('favorited', { detail: btnFav.classList.contains('active') }));
 		});
 		this.shadowRoot.getElementById('Refresh').addEventListener('click', () => {
 			this.dispatchEvent(new Event('refresh'));
@@ -31,8 +33,12 @@ class UrlBar extends HTMLElement {
 			}
 		});
 		txtUrlBar.addEventListener('focusout', () => {
+			this.shadowRoot.querySelector('.urlOuter').classList.remove('urlFocus');
 			this.shadowRoot.getElementById('URLAutoComplete').classList.add('AutoComplete_hidden');
 			txtUrlBar.value = txtUrlBar.getAttribute('data-text-original');
+		});
+		txtUrlBar.addEventListener('focus', () => {
+			this.shadowRoot.querySelector('.urlOuter').classList.add('urlFocus');
 		});
 		txtUrlBar.addEventListener('keydown', (e) => {
 			this.shadowRoot.getElementById('URLAutoComplete').classList.remove('AutoComplete_hidden');
@@ -46,6 +52,20 @@ class UrlBar extends HTMLElement {
 		txtUrlBar.value = friendly;
 		txtUrlBar.setAttribute('data-text-swap', raw);
 		txtUrlBar.setAttribute('data-text-original', friendly);
+		this.updateCertBtn();
+	}
+	updateCertBtn(){
+		let rawURL = this.shadowRoot.getElementById('URLBar').getAttribute('data-text-swap').toLowerCase();
+		let certBtn = this.shadowRoot.getElementById('SecureCheck');
+		certBtn.classList.remove('Http', 'Https', 'CirtError', 'Loading', 'Internal');
+		switch(true) {
+		case rawURL.startsWith('https://'):
+			certBtn.classList.add('Https');
+			break;
+		case rawURL.startsWith('http://'):
+			certBtn.classList.add('Http');
+			break;
+		}
 	}
 }
 
