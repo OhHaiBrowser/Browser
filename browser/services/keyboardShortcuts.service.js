e05c8825a5315180d2163e4bf89206f56c71facb
Lucas Reade <lucas.reade@yahoo.com>
2020-05-19 21:20:27 +0100

    Update keyboardShortcuts.service.js

--- browser/services/keyboardShortcuts.service.js
+++ browser/services/keyboardShortcuts.service.js
@@ -1,7 +1,18 @@
 const Mousetrap = require('mousetrap');
 
 module.exports.initShortcuts = () => {
+	// Show/Hide history
+	Mousetrap.bind(['command+h', 'ctrl+h'], () => {
 
+	});
+	// Show/Hide bookmarks
+	Mousetrap.bind(['command+b', 'ctrl+b'], () => {
 
+	});
+	// Show/Hide tabs
 
+	// New tab
+	Mousetrap.bind(['command+t', 'ctrl+t'], () => {
+
+	});
 };
