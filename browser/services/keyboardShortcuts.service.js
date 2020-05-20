ca6a7670da85cc06fa1ca2214d061d75134c8983
Lucas Reade <lucas.reade@yahoo.com>
2020-05-20 11:27:18 +0100

    Updates to shortcuts

--- browser/services/keyboardShortcuts.service.js
+++ browser/services/keyboardShortcuts.service.js
@@ -1,18 +1,5 @@
-const Mousetrap = require('mousetrap');
+const { globalShortcut } = require('electron')
 
 module.exports.initShortcuts = () => {
-	// Show/Hide history
-	Mousetrap.bind(['command+h', 'ctrl+h'], () => {
 
-	});
-	// Show/Hide bookmarks
-	Mousetrap.bind(['command+b', 'ctrl+b'], () => {
-
-	});
-	// Show/Hide tabs
-
-	// New tab
-	Mousetrap.bind(['command+t', 'ctrl+t'], () => {
-
-	});
 };
