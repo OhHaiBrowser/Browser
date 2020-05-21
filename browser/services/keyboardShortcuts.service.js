56bdd01db55874781c05eddb7b62c8194f4798b5
Lucas Reade <lucas.reade@yahoo.com>
2020-05-21 15:57:51 +0100

    Update keyboardShortcuts.service.js

--- browser/services/keyboardShortcuts.service.js
+++ browser/services/keyboardShortcuts.service.js
@@ -1,13 +1,5 @@
-const { globalShortcut, remote } = require('electron');
-const thisWindow = remote.getCurrentWindow();
 
 module.exports.initShortcuts = () => {
 
-	globalShortcut.register('CommandOrControl+B', () => {
-		if(thisWindow.isFocused()) {
-			const sideBar = document.getElementById('Sid');
-			sideBar.showPanel(sideBar.panels().item(1));
-		}
-	});
 
 };
